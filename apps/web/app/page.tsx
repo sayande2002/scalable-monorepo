bf940ad936a631a65ed74188d7c94a8c4d2eafd2
Sayan De <sayandeten@gmail.com>
2023-08-10 23:57:21 +0530

    second commit

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,6 +5,12 @@ export default function Page() {
     <>
       <Header text="Web" />
       <Button />
+      <h1>{process.env.APP_ENV}</h1>
+      <h1>{process.env.APP_ENV_TWO}</h1>
+      <h1>{process.env.PRISMA_ENV}</h1>
+      <h1>{process.env.PRISMA_ENV_TWO}</h1>
+      <h1>{process.env.GLOBAL_ENV}</h1>
+      <h1>{process.env.GLOBAL_ENV_TWO}</h1>
     </>
   );
 }
