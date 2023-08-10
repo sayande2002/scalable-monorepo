87588821528f31b4d6f3b20a3f65d83c973a8cb5
Sayan De <sayandeten@gmail.com>
2023-08-11 01:52:17 +0530

    remove test

--- packages/prisma/test.js
+++
@@ -1,6 +0,0 @@
-console.log("process.env.APP_ENV", process.env.APP_ENV);
-console.log("process.env.APP_ENV", process.env.APP_ENV_TWO);
-console.log("process.env.APP_ENV", process.env.PRISMA_ENV);
-console.log("process.env.APP_ENV", process.env.PRISMA_ENV_TWO);
-console.log("process.env.APP_ENV", process.env.GLOBAL_ENV);
-console.log("process.env.GLOBAL_ENV_TWO", process.env.GLOBAL_ENV_TWO);
